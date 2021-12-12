c22fdd3779cb457b0bfdb3a1b390e1208cf69c8a
shn2002 <joe.shanhenian@gmail.com>
2021-12-12 18:40:43 -0500

    second

--- webhook.js
+++ webhook.js
@@ -1,6 +1,6 @@
 let http =require('http');
-let server = http.createserver (function (req, res){
-    console.log(req.ethod,req.url);
+let server = http.createServer (function (req, res){
+    console.log(req.method,req.url);
     if (req.method =='POST' && req.url == '/webhook'){
         res.setHeader('Content-Type','application/json');
         res.end(JSON.stringify({ok:true}));
