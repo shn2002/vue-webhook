01cfde3e1eb60fc8823e949e713b769ebbbf9e6c
shn2002 <joe.shanhenian@gmail.com>
2021-12-13 00:38:36 -0500

    comment

--- vue-back.sh
+++ vue-back.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 WORK_PATH ='user/projects/vue-back'
 cd $WORK_PATH
-echo "先清楚老代码"
+echo "先清除老代码"
 git reset --hard prign/master
 git clean -f
 echo "拉取最新代码"
 git pull origin master
 echo "开始执行构建"
-docker build -t vue-back .
+docker build -t vue-back:1.0 .
 echo "先删除旧容器"
 docker stop vue-back-container
 docker rm vue-back-container
 echo "启动新容器"
-docker container run p 3000:3000 --name vue-back-container -d vue-back
+docker container run p 3000:3000 --name vue-back-container -d vue-back:1.0
